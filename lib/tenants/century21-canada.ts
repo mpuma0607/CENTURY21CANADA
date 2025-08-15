ca176ca3d184185776380eb6ee811b65d3c78c43
Mike Puma <mpuma0607@gmail.com>
2025-08-15 07:59:46 -0400

    fix: Rename logo file and update tenant config to fix image loading - Renamed logo file to remove special characters (spaces and parentheses) - Updated tenant configuration to use new filename - Logo should now load properly in navigation

--- lib/tenants/century21-canada.ts
+++ lib/tenants/century21-canada.ts
@@ -10,8 +10,8 @@ export const century21CanadaConfig: TenantConfig = {
   ],
   branding: {
     name: "Century 21 Canada",
-    logo: "/images/logo_full_gold_1000X232(1).png",
-    logoDark: "/images/logo_full_gold_1000X232(1).png",
+    logo: "/images/century21-canada-logo-gold.png",
+    logoDark: "/images/century21-canada-logo-gold.png",
     colors: {
       primary: "#B6A888", // Vegas Gold
       secondary: "#000000", // Black
